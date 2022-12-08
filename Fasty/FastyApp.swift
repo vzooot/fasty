d1a1bd1bdaba1e6f2e62763813af31e08730ff9b
vzotikas <vzotikas@vzotikas.com>
2022-12-08 22:22:23 +0100

    Initial Commit

---
+++ Fasty/FastyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastyApp.swift
+//  Fasty
+//
+//  Created by Administrator on 12/8/22.
+//
+
+import SwiftUI
+
+@main
+struct FastyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
